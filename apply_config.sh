0b3e41883110888dfd50c4ed4dd8e326038b6c51
Vincent Duong <vincent.duong.dev@gmail.com>
2021-05-15 08:24:46 +0000

    feat(xorg-config): add script to sync file

---
+++ apply_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x #echo on
+
+# Xorg configuration (usefull for keyboard configuration) 
+cp 00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf 
---
+++ copy_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Xorg configuration (usefull for keyboard configuration) 
+cp /etc/X11/xorg.conf.d/00-keyboard.conf .
