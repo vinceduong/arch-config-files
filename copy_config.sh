13d402e55586fa79c77360cf5c147685a9c4c670
Vincent Duong <vincent.duong.dev@gmail.com>
2021-05-15 08:49:46 +0000

    refactor(files): create a file directory

--- copy_config.sh
+++ copy_config.sh
@@ -2,7 +2,7 @@
 set -x #echo on
 
 # Xorg configuration (usefull for keyboard configuration) 
-cp /etc/X11/xorg.conf.d/00-keyboard.conf .
+cp /etc/X11/xorg.conf.d/00-keyboard.conf ./files
 
 # i3 config file
-cp ~/.i3/config ./i3.config
+cp ~/.i3/config ./files/i3.config
