d37a87286aae2b219a102db63a375801082953ee
Vincent Duong <vincent.duong.dev@gmail.com>
2021-05-15 08:40:10 +0000

    feat(i3): add i3 config file

--- apply_config.sh
+++ apply_config.sh
@@ -3,3 +3,6 @@ set -x #echo on
 
 # Xorg configuration (usefull for keyboard configuration) 
 cp 00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf 
+
+# i3 config file
+cp i3.config /home/vince/.i3/config 
--- copy_config.sh
+++ copy_config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -x #echo on
 
 # Xorg configuration (usefull for keyboard configuration) 
 cp /etc/X11/xorg.conf.d/00-keyboard.conf .
+
+# i3 config file
+cp ~/.i3/config ./i3.config
